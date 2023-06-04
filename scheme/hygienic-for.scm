703229ecdfffc2439721081943916b647e8eef8a
Alexander Lelchuk <alex0surname@gmail.com>
2023-06-04 12:36:38 +0300

    upd

--- scheme/hygienic-for.scm
+++ scheme/hygienic-for.scm
@@ -1,6 +1,6 @@
 (load "functional.scm")
 
-(define-syntax for1 ; (for1 i '(1 2 3) (+ 2 (* i 10)))
+(define-syntax for- ; (for- i '(1 2 3) (+ 2 (* i 10)))
   (syntax-rules ()
     [(_ var seq . body)
       (map (lambda (var) . body) seq)]))
@@ -10,18 +10,22 @@
     [(_ (var seq) . body)
       (map (lambda (var) . body) seq)]))
 
-(define (group2 lst)
-  (if (null? lst) '()
-    (cons (list (car lst) (cadr lst)) (group2 (cddr lst)))))
+(define-syntax for*- ; (for*- ((i '(1 2 3))) i)  ; (for*- ((i '(1 2 3)) (j '(1 2 3))) (* i j))
+  (syntax-rules ()
+    [(_ ((var range)) . body)
+     (for (var range) . body)]
+    [(_ ((var range) . bindings) . body)
+     (mapcan 
+       (lambda (var)
+        (for*- bindings . body))
+       range)]))
 
-
-(define-macro (for*- bindings . body)   ; (for*- ((i '(1 2 3))) i)  ; (for*- ((i '(1 2 3)) (j '(1 2 3))) (* i j))
-  (if (= 1 (length bindings))
-    `(for ,(car bindings) ,@body)
-    `(mapcan 
-      (lambda (,(caar bindings))
-        (for*- ,(cdr bindings) ,@body))
-      ,(cadar bindings)))) 
-
-(define-macro (for* bindings . body)   ; (for* (i '(1 2 3) j '(1 2 3)) (* i j))
-  `(for*- ,(group2 bindings) ,@body))
+(define-syntax for* ; (for* (i '(1 2 3) j '(1 2 3)) (* i j))
+  (syntax-rules ()
+    [(_ (var range) . body)
+     (for (var range) . body)]
+    [(_ (var range . bindings) . body)
+     (mapcan 
+       (lambda (var)
+        (for* bindings . body))
+       range)]))
